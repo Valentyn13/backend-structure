f471a5403716911db28f0a93e2676108241c04cc
Valentyn13 <valentyn13git@gmail.com>
2024-01-27 20:04:38 +0200

    create stats api

---
+++ src/api/stats/stats.api.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const stats = {
+    totalUsers: 4,
+    totalBets: 1,
+    totalEvents: 1,
+  };
+
+const initStats = (Router) => {
+    const router = new Router()
+
+    router.get('/',(req, res) => {
+        try {
+          var ak = 'authorization';
+          let token = req.headers[ak];
+          if(!token) {
+            return res.status(401).send({ error: 'Not Authorized' });
+          }
+          token = token.replace('Bearer ', '');
+          try {
+            var tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+            if (tokenPayload.type != 'admin') {
+              throw new Error();
+            }
+          } catch (err) {
+            return res.status(401).send({ error: 'Not Authorized' });
+          }
+          res.send(stats);
+        } catch (err) {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+      })
+
+    return router
+}
+
+module.exports = initStats
