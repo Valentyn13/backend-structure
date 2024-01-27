7667a301ce5ceec06adb277f9cdb50ea59e9f1b7
Valentyn13 <valentyn13git@gmail.com>
2024-01-27 20:06:16 +0200

    create initApi router

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+const apiPath = require('../common/enums/api-path.enum')
+const initHealth = require('./health/health.api')
+const initUsers = require('./users/users.api')
+const initTransactions = require('./transactions/transactions.api')
+const initEvents = require('./events/events.api')
+const initBets = require('./bets/bets.api')
+const initStats = require('./stats/stats.api')
+
+const initApi = Router => {
+    const apiRouter = new Router()
+
+    apiRouter.use(apiPath.HEALTH,initHealth(Router))
+    apiRouter.use(apiPath.USERS, initUsers(Router))
+    apiRouter.use(apiPath.TRANSACTIONS, initTransactions(Router))
+    apiRouter.use(apiPath.EVENTS,initEvents(Router))
+    apiRouter.use(apiPath.BETS, initBets(Router))
+    apiRouter.use(apiPath.STATS,initStats(Router))
+
+
+
+    return apiRouter
+}
+
+module.exports = initApi
