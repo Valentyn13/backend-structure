f37b3c520bc5a4d84e43c6843abdd75aeeecb867
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 16:11:01 +0200

    create userService

---
+++ src/services/user.service.js
@@ -0,0 +1,37 @@
+const userRepository = require('../data/repositories/user.repository')
+const statEmitter = require('../socket/connection')
+
+class User {
+    constructor(userRepository) {
+        this._userRepository = userRepository
+    }
+
+    async getUserById(id) {
+        const result =  await this._userRepository.getById(id)
+        return result[0]
+    }
+
+    async createUser(data) {
+        const result = await this._userRepository.create(data)
+        const user = result[0]
+
+        user.createdAt = user.created_at;
+        delete user.created_at;
+        user.updatedAt = user.updated_at;
+        delete user.updated_at;
+        statEmitter.emit('newUser');
+    
+        return user
+    }
+
+    async updateUser (id,data) {
+        const result = await this._userRepository.update(id,data)
+        return result
+    }
+
+}
+
+
+const userService = new User(userRepository)
+
+module.exports = userService
