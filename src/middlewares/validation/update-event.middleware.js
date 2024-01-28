f3df6b5c9f11dc81e31028b7184024f7e505fe65
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 20:18:51 +0200

    add updateEventValidator

---
+++ src/middlewares/validation/update-event.middleware.js
@@ -0,0 +1,16 @@
+const joi = require('joi');
+
+const updateEventValidator = (req,res,next) => {
+    const schema = joi.object({
+        score: joi.string().required(),
+      }).required();
+      const isValidResult = schema.validate(req.body);
+      if(isValidResult.error) {
+        res.status(400).send({ error: isValidResult.error.details[0].message });
+        return;
+      };
+
+      next()
+}
+
+module.exports = updateEventValidator
