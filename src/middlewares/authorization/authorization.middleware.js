23f672d67bad3c389cd4b340f6b12e23403b879a
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 12:00:01 +0200

    create authorizationMiddleware

---
+++ src/middlewares/authorization/authorization.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const authorizationMiddleware = (req, res, next) => {
+  let tokenPayload;
+  let token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).send({ error: "Not Authorized" });
+  }
+
+  token = token.replace("Bearer ", "");
+
+  try {
+    tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).send({ error: "Not Authorized" });
+  }
+
+  if (req.params.id !== tokenPayload.id) {
+    return res.status(401).send({ error: "UserId mismatch" });
+  }
+  next()
+};
+
+module.exports = authorizationMiddleware
