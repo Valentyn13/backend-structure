9a3d7f72bdbfb3073852a2506c31981f685f9238
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 18:50:01 +0200

    add authorization middleware to stats router

--- src/api/stats/stats.api.js
+++ src/api/stats/stats.api.js
@@ -1,31 +1,11 @@
-const jwt = require("jsonwebtoken");
+const adminAuthorizationMiddleware = require('../../middlewares/authorization/admin-authorization.middleware')
 const stats = require('../../common/constants/constants')
 
 const initStats = (Router) => {
     const router = new Router()
 
-    router.get('/',(req, res) => {
-        try {
-          var ak = 'authorization';
-          let token = req.headers[ak];
-          if(!token) {
-            return res.status(401).send({ error: 'Not Authorized' });
-          }
-          token = token.replace('Bearer ', '');
-          try {
-            var tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
-            if (tokenPayload.type != 'admin') {
-              throw new Error();
-            }
-          } catch (err) {
-            return res.status(401).send({ error: 'Not Authorized' });
-          }
-          res.send(stats);
-        } catch (err) {
-          console.log(err);
-          res.status(500).send("Internal Server Error");
-          return;
-        }
+    router.get('/',adminAuthorizationMiddleware,(req, res) => {
+        res.send(stats);
       })
 
     return router
--- src/middlewares/authorization/admin-authorization.middleware.js
+++ src/middlewares/authorization/admin-authorization.middleware.js
@@ -4,18 +4,25 @@ const adminAuthorizationMiddleware = (req, res, next) => {
   let tokenPayload;
   let token = req.headers["authorization"];
 
-  if (!token) {
-    return res.status(401).send({ error: "Not Authorized" });
+  try {
+    if (!token) {
+      return res.status(401).send({ error: "Not Authorized" });
+    }
+  
+    token = token.replace("Bearer ", "");
+  
+    tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+  
+    if (tokenPayload.type != "admin") {
+      return res.status(401).send({ error: "Not Authorized" });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+    return;
   }
 
-  token = token.replace("Bearer ", "");
-
-  tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
-
-  if (tokenPayload.type != "admin") {
-    return res.status(401).send({ error: "Not Authorized" });
-  }
-  next();
 };
 
 
