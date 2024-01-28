138e951ed7967821c7d6c171580cacd9ab89b6f0
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 17:27:37 +0200

    add createTransactionMiddleware

---
+++ src/middlewares/validation/create-transaction.middleware.js
@@ -0,0 +1,19 @@
+const joi = require('joi');
+
+const createTransactionValidator = (req, res, next) => {
+    const schema = joi.object({
+        id: joi.string().uuid(),
+        userId: joi.string().uuid().required(),
+        cardNumber: joi.string().required(),
+        amount: joi.number().min(0).required(),
+      }).required();
+      const isValidResult = schema.validate(req.body);
+      if(isValidResult.error) {
+        res.status(400).send({ error: isValidResult.error.details[0].message });
+        return;
+      };
+    next()
+}
+
+
+module.exports = createTransactionValidator
