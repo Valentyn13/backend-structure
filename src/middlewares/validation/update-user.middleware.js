b51ed20fb4f0936f2ccdd5d40d620f61c2c0ea30
Valentyn13 <valentyn13git@gmail.com>
2024-01-28 11:58:39 +0200

    create updateUserValidator

---
+++ src/middlewares/validation/update-user.middleware.js
@@ -0,0 +1,25 @@
+const joi = require('joi');
+
+const updateUserValidator = (req, res, next) => {
+  try {
+    var schema = joi
+      .object({
+        email: joi.string().email(),
+        phone: joi.string().pattern(/^\+?3?8?(0\d{9})$/),
+        name: joi.string(),
+        city: joi.string(),
+      })
+      .required();
+
+    var isValidResult = schema.validate(req.body);
+    if (isValidResult.error) {
+      res.status(400).send({ error: isValidResult.error.details[0].message });
+      return;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = updateUserValidator
